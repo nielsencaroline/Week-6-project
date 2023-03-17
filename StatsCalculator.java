38d8d6b411d51de7bd7ed209860860c48b62f34e
nielsencaroline <113924836+nielsencaroline@users.noreply.github.com>
2023-03-17 09:08:18 -0700

    Update StatsCalculator.java
    updated methods

--- StatsCalculator.java
+++ StatsCalculator.java
@@ -1,3 +1,26 @@
+
+Week 6 Programming Assignment - Final Version
+Brandon Smith
+•
+Mar 8
+100 points
+Due 8:30 AM
+
+Week 6 Programming Assignment
+Google Docs
+Class comments
+Your work
+Missing
+
+Caroline Nielsen - Week 6 Programming Assignment Rubric
+Google Sheets
+
+GitHub - nielsencaroline/Week-6-project
+https://github.com/nielsencaroline/Week-6-project
+
+StatsCalculator.java
+Java
+Private comments
 import java.util.Arrays;
 /**
  * @file StatsCalculator.java
@@ -6,83 +29,82 @@ import java.util.Arrays;
  *
  * @Description uses an array to calculate minimum, first quartile, median, third quartile, maximum, and mean
  */
-
 public class StatsCalculator {
-    /**
-     * @description creates double array of values
-     */
-        private double[] arr;
-    /**
-     * @description creates double array of sorted numbers
-     */
-        private double[] sortedArr;
+        /**
+        * @description creates double array of values
+        */
+        private double[] value;
+        /**
+        * @description creates double array of sorted numbers
+        */
+        private double[] sortedValues;
 
         /**
          * Default constructor that creates an array of 20 zeros
          */
     public StatsCalculator(){
-            arr = new double[20];
-            sortedArr = new double[20];
-            Arrays.sort(sortedArr);
+            value = new double[20];
+            sortedValues = new double[20];
+            Arrays.sort(sortedValues);
         }
-    /**
-     * Constructor that creates a StatsCalculator object with a user defined array
-     *
-     * @param arr The array input by the user
-     */
+        /**
+         * Constructor that creates a StatsCalculator object with a user defined array
+         *
+        * @param value The array input by the user
+        */
 
-    public StatsCalculator(double[] arr){
-             this.arr = arr;
-             sortedArr = arr;
+    public StatsCalculator(double[] value){
+             this.value = value;
+             sortedValues = value;
         }
-    /**
-     * Sorts the values in increasing direction
-     * Java.util.Arrays.copyOf() method returns a copy of the original array
-     * @return sorted numbers
-     */
+        /**
+         * Sorts the values in increasing direction
+         * Java.util.Arrays.copyOf() method returns a copy of the original array
+        * @return sorted numbers
+        */
 
     public void sortData(){
-        sortedArr = Arrays.copyOf(arr, arr.length);
-        Arrays.sort(sortedArr);
+        sortedValues = Arrays.copyOf(value, value.length);
+        Arrays.sort(sortedValues);
     }
-    /**
-     * @description calculates the max in the sorted array
-     * @return max
-     */
+        /**
+        * @description calculates the max in the sorted array
+        * @return max
+        */
     public double calculateMax(){
-            double max = arr[0];
-            for (double num : arr) {
+            double max = value[0];
+            for (double num : value) {
                  if (num >= max) {
                     max = num;
                  }
             }
             return max;
         }
-    /**
-     * @description calculates the min of the sorted array
-     * @return min
-     */
+        /**
+        * @description calculates the min of the sorted array
+        * @return min
+        */
        public double calculateMin(){
-           double min = arr[0];
-           for (double num : arr) {
+           double min = value[0];
+           for (double num : value) {
                if (num < min) {
                    min = num;
                }
            }
            return min;
         }
-    /**
-     * @description calculates the median
-     * @return median
-     */
+        /**
+        * @description calculates the median
+        * @return median
+        */
     public double calculateMedian(){
         sortData();
         double median = 0;
-        if (sortedArr.length % 2 == 1){
-            median = sortedArr[sortedArr.length / 2];
+        if (sortedValues.length % 2 == 1){
+            median = sortedValues[sortedValues.length / 2];
         }
         else{
-            median =  (sortedArr[(sortedArr.length - 1) / 2] + sortedArr[sortedArr.length / 2]) / 2.0;
+            median =  (sortedValues[(sortedValues.length - 1) / 2] + sortedValues[sortedValues.length / 2]) / 2.0;
         }
         return median;
     }
@@ -92,7 +114,13 @@ public class StatsCalculator {
      */
     public double calculateFirstQuartile() {
         sortData();
-        double firstQuartile = sortedArr[(int) (sortedArr.length * 0.25)];
+        double firstQuartile = -1;
+        if(sortedValues.length % 4 == 0){
+            firstQuartile = (sortedValues[(sortedValues.length / 4) - 1] + sortedValues[(sortedValues.length / 4)]) / 2;
+        }
+        else{
+            firstQuartile = sortedValues[sortedValues.length / 4];
+        }
         return firstQuartile;
     }
     /**
@@ -101,56 +129,65 @@ public class StatsCalculator {
      */
     public double calculateThirdQuartile() {
         sortData();
-        double thirdQuartile = sortedArr[(int) (sortedArr.length * 0.75)];
+        double thirdQuartile = -1;
+        if(sortedValues.length % 2 == 0) {
+           thirdQuartile = (sortedValues[(sortedValues.length / 4) + (sortedValues.length / 2) - 1] + sortedValues[(sortedValues.length / 4) + (sortedValues.length / 2)]) / 2;
+        }
+        else if(sortedValues.length % 4 == 0) {
+            thirdQuartile = sortedValues[(int) (sortedValues.length * .75)-1];
+        }
+        else{ // odd length
+            thirdQuartile = sortedValues[(sortedValues.length / 4) + (sortedValues.length/2)+1];
+        }
         return thirdQuartile;
     }
 
-    /**
-     * @description calculates the sum of the sorted array
-     * @return sum
-     */
+        /**
+        * @description calculates the sum of the sorted array
+        * @return sum
+        */
         public double calculateSum(){
             int sum = 0;
-            for (int i = 0; i < arr.length; i++) {
-                sum += arr[i];
+            for (int i = 0; i < value.length; i++) {
+                sum += value[i];
             }
             return sum;
         }
-    /**
-     * @description calculates the mean of the sorted array
-     * @return mean
-     */
-        public double calculateMean() {
+        /**
+         * @description calculates the mean of the sorted array
+         * @return mean
+        */
+         public double calculateMean() {
             double total = 0;
-            for(int i = 0; i < arr.length; i++){
-                total = total + arr[i];
+            for(int i = 0; i < value.length; i++){
+                total = total + value[i];
             }
-            double average = total / arr.length;
+            double average = total / value.length;
             return average;
         }
-    /**
-     * @description prints list of unsorted numbers
-     */
+        /**
+         * @description prints list of unsorted numbers
+         */
         public void print() {
-            for (double value : arr) {
+            for (double value : value) {
                 System.out.print(value);
                 System.out.print(" ");
             }
         }
-    /**
-     * @description prints list of sorted numbers
-     */
-        public void printSorted(){
-            sortData();
-                for (double value : sortedArr)
-                {
-                    System.out.print(value);
-                    System.out.print(" ");
-            }
+        /**
+         * @description prints list of sorted numbers
+         */
+    public void printSorted(){
+        sortData();
+        for (double value : sortedValues)
+        {
+            System.out.print(value);
+            System.out.print(" ");
         }
-    /**
-     * @description prints five number summary
-     */
+    }
+        /**
+         * @description prints five number summary
+        */
         public void printFiveNumberSummary() {
             System.out.println("\t Minimum: " + calculateMin());
             System.out.println("\t First quartile: " + calculateFirstQuartile());
